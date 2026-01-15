be18fd96ab1e9d5f1b7cf3de1c344a653c4f9d40
IL0021 <arpit.chaurasia@innovate-labs.com>
2026-01-15 19:10:30 +0530

    implemented delete and archive

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,16 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.TestTools;
+using System;
+using System.IO.Compression;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    public List<string> unityProjects = new List<string>();
+    public List<ProjectData> unityProjects = new List<ProjectData>();
+    private Coroutine _searchCoroutine;
     void Awake()
     {
         if (Instance == null)
@@ -30,34 +35,159 @@ public class GameManager : MonoBehaviour
             return;
         }
 
-        FindUnityProjectsRecursively(sourcePath);
+        if (_searchCoroutine != null) StopCoroutine(_searchCoroutine);
+        _searchCoroutine = StartCoroutine(FindUnityProjectsRecursively(sourcePath));
+    }
 
-        foreach (string projectPath in unityProjects)
+    private IEnumerator FindUnityProjectsRecursively(string rootPath)
+    {
+        Stack<SearchNode> stack = new Stack<SearchNode>();
+        stack.Push(new SearchNode(rootPath, 0f, 1f));
+
+        float timeBudget = 0.005f;
+        float startTime = Time.realtimeSinceStartup;
+
+        while (stack.Count > 0)
         {
-            UIHandler.Instance.AddToList(projectPath);
+            SearchNode currentNode = stack.Pop();
+            string currentDir = currentNode.Path;
+
+            // Update UI with current progress (0 to 100)
+            UIHandler.Instance.UpdateProgress(currentNode.MinProgress * 100f);
+
+            if (Directory.Exists(Path.Combine(currentDir, "Assets"))
+                && Directory.Exists(Path.Combine(currentDir, "ProjectSettings"))
+                && Directory.Exists(Path.Combine(currentDir, "Packages"))
+                && Directory.Exists(Path.Combine(currentDir, "UserSettings")))
+            {
+                ProjectData projectData = new ProjectData(currentDir, ActionType.None);
+                unityProjects.Add(projectData);
+                UIHandler.Instance.AddToList(projectData);
+                continue;
+            }
+
+            try
+            {
+                string[] subDirectories = Directory.GetDirectories(currentDir);
+                int subDirCount = subDirectories.Length;
+
+                if (subDirCount > 0)
+                {
+                    float step = (currentNode.MaxProgress - currentNode.MinProgress) / subDirCount;
+                    // Iterate backwards to push to stack so we pop/process them in order
+                    for (int i = subDirCount - 1; i >= 0; i--)
+                    {
+                        float childMin = currentNode.MinProgress + (i * step);
+                        float childMax = currentNode.MinProgress + ((i + 1) * step);
+                        stack.Push(new SearchNode(subDirectories[i], childMin, childMax));
+                    }
+                }
+            }
+            catch (System.Exception e) { Debug.LogWarning($"Could not access directory: {currentDir}. Reason: {e.Message}"); }
+
+            if (Time.realtimeSinceStartup - startTime > timeBudget)
+            {
+                yield return null;
+                startTime = Time.realtimeSinceStartup;
+            }
+        }
+
+        // Reaching 100%
+        UIHandler.Instance.UpdateProgress(100f);
+        Debug.Log("Search Complete");
+        UIHandler.Instance.continueButton.interactable = true;
+    }
+
+    public void PerformActionsOnProjects()
+    {
+        foreach (var project in unityProjects)
+        {
+            switch (project.actionType)
+            {
+                case ActionType.Archive:
+                    Debug.Log($"Archiving project: {project.projectPath}");
+                    StartCoroutine(ArchiveProject(project.projectPath));
+                    break;
+                case ActionType.Delete:
+                    Debug.Log($"Deleting project: {project.projectPath}");
+                    StartCoroutine(DeleteProject(project.projectPath));
+                    break;
+                case ActionType.None:
+                    Debug.Log($"No action for project: {project.projectPath}");
+                    break;
+            }
         }
     }
 
-    private void FindUnityProjectsRecursively(string directory)
+    private IEnumerator DeleteProject(string projectPath)
     {
-        if (Directory.Exists(Path.Combine(directory, "Assets"))
-            && Directory.Exists(Path.Combine(directory, "ProjectSettings"))
-            && Directory.Exists(Path.Combine(directory, "Packages"))
-            && Directory.Exists(Path.Combine(directory, "UserSettings")))
-        {
-            unityProjects.Add(directory);
-            UIHandler.Instance.AddToList(directory);
-            Debug.Log($"Found Unity project: {directory}");
-            return;
-        }
+        Directory.Delete(projectPath, true);
+        yield return null;
+    }
 
-        try
+    private IEnumerator ArchiveProject(string projectPath)
+    {
+        projectPath = projectPath.TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+        string parentDir = Path.GetDirectoryName(projectPath);
+        string archivePath = Path.Combine(parentDir, Path.GetFileName(projectPath) + ".zip");
+
+        string[] foldersToInclude = { ".git", "Assets", "ProjectSettings", "Packages", "UserSettings" };
+
+        using (FileStream zipToOpen = new FileStream(archivePath, FileMode.Create))
         {
-            foreach (string subDirectory in Directory.GetDirectories(directory))
+            using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Create))
             {
-                FindUnityProjectsRecursively(subDirectory);
+                foreach (string folder in foldersToInclude)
+                {
+                    string sourceDir = Path.Combine(projectPath, folder);
+                    if (Directory.Exists(sourceDir))
+                    {
+                        string[] files = Directory.GetFiles(sourceDir, "*", SearchOption.AllDirectories);
+                        foreach (string file in files)
+                        {
+                            string relativePath = file.Substring(projectPath.Length + 1).Replace('\\', '/');
+                            archive.CreateEntryFromFile(file, relativePath);
+                        }
+                    }
+                }
             }
         }
-        catch (System.Exception e) { Debug.LogWarning($"Could not access directory: {directory}. Reason: {e.Message}"); }
+
+        Directory.Delete(projectPath, true);
+        yield return null;
+    }
+
+    private struct SearchNode
+    {
+        public string Path;
+        public float MinProgress;
+        public float MaxProgress;
+
+        public SearchNode(string path, float min, float max)
+        {
+            Path = path;
+            MinProgress = min;
+            MaxProgress = max;
+        }
+    }
+}
+[Serializable]
+public enum ActionType
+{
+    None,
+    Archive,
+    Delete
+}
+
+[System.Serializable]
+public class ProjectData
+{
+    public string projectPath;
+    public ActionType actionType;
+
+    public ProjectData(string path, ActionType action)
+    {
+        projectPath = path;
+        actionType = action;
     }
 }
--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.IO;
 public class UIHandler : MonoBehaviour
 {
     public static UIHandler Instance;
-    GameObject sourcePanel, listPanel;
+    GameObject sourcePanel, listPanel, dialogPanel;
     TMP_InputField sourceInputField;
-    Button startButton;
+    Button startButton, backButton, yes, no;
+    public Button continueButton;
+    GameObject card;
+    Transform listContent;
+    Slider scanSlider;
 
     void Awake()
     {
@@ -23,6 +28,7 @@ public class UIHandler : MonoBehaviour
 
     void Start()
     {
+        card = Resources.Load<GameObject>("Card");
         GetReferences();
         AssignListeners();
     }
@@ -34,13 +40,55 @@ public class UIHandler : MonoBehaviour
 
         sourcePanel = transform.GetChildWithName("SourcePanel").gameObject;
         listPanel = transform.GetChildWithName("ListPanel").gameObject;
+
+        listContent = listPanel.transform.GetChildWithName("ListContent");
+        continueButton = listPanel.transform.GetChildWithName("ContinueButton").GetComponent<Button>();
+        continueButton.interactable = false;
+        backButton = listPanel.transform.GetChildWithName("BackButton").GetComponent<Button>();
+        scanSlider = listPanel.transform.GetChildWithName("ScanSlider").GetComponent<Slider>();
+
+
+        dialogPanel = transform.GetChildWithName("DialogPanel").gameObject;
+        yes = dialogPanel.transform.GetChildWithName("YesButton").GetComponent<Button>();
+        no = dialogPanel.transform.GetChildWithName("NoButton").GetComponent<Button>();
     }
 
     private void AssignListeners()
     {
         startButton.onClick.AddListener(OnStartButtonClicked);
+        continueButton.onClick.AddListener(OnContinueButtonClicked);
+        backButton.onClick.AddListener(() =>
+        {
+            listPanel.SetActive(false);
+            sourcePanel.SetActive(true);
+            GameManager.Instance.unityProjects.Clear();
+            foreach (Transform child in listContent)
+            {
+                Destroy(child.gameObject);
+            }
+        });
+        yes.onClick.AddListener(OnYesButtonClicked);
+        no.onClick.AddListener(OnNoButtonClicked);
     }
 
+    private void OnYesButtonClicked()
+    {
+        "Yes button clicked.".Print();
+        dialogPanel.SetActive(false);
+        GameManager.Instance.PerformActionsOnProjects();
+    }
+
+    private void OnNoButtonClicked()
+    {
+        "No button clicked.".Print();
+        dialogPanel.SetActive(false);
+    }
+
+    private void OnContinueButtonClicked()
+    {
+        Debug.Log("Continue button clicked.");
+        dialogPanel.SetActive(true);
+    }
     private void OnStartButtonClicked()
     {
         GameManager.Instance.StartSearch(sourceInputField.text);
@@ -48,8 +96,32 @@ public class UIHandler : MonoBehaviour
         listPanel.SetActive(true);
     }
 
-    public void AddToList(string projectName)
+    public void AddToList(ProjectData projectData)
     {
-        
+        GameObject newCard = Instantiate(card, listContent);
+
+        Button cardButton = newCard.GetComponentInChildren<Button>();
+
+        TextMeshProUGUI projectNameText = newCard.transform.GetChildWithName("ProjectName").GetComponent<TextMeshProUGUI>();
+        projectNameText.text = Path.GetFileName(projectData.projectPath);
+
+        cardButton.onClick.AddListener(() =>
+        {
+            $"Clicked on project: {projectData.projectPath}".Print();
+        });
+
+        TMP_Dropdown dropdown = newCard.GetComponentInChildren<TMP_Dropdown>();
+        dropdown.onValueChanged.AddListener((int index) =>
+        {
+            $"Selected option {index} for project: {projectData.projectPath}".Print();
+            projectData.actionType = (ActionType)index;
+        });
     }
+
+    public void UpdateProgress(float value)
+    {
+        if (scanSlider != null)
+            scanSlider.value = value;
+    }
+
 }
